7444dfe821076f99479a4b4f823808509075f319
Lukrial <54103585+Lukrial@users.noreply.github.com>
2021-04-21 11:04:23 +0200

    funny haha playerpieces works not stinky

--- DDU-Eksamen1/src/main/java/Soeroever.java
+++ DDU-Eksamen1/src/main/java/Soeroever.java
@@ -1,45 +1,12 @@
 import processing.core.PApplet;
 import processing.core.PImage;
 
-public class Soeroever {
-    PApplet p;
-    int posX;
-    int posY;
-    boolean pieceHeld=false;
+public class Soeroever extends PlayerPieces{
 
     Soeroever(int posX, int posY, PApplet p){
+        super(p, posX, posY);
         this.posX = posX;
         this.posY = posY;
-        this.p=p;
-    }
-
-    void drawBoardPiece(PImage boardPiece){
-        p.image(boardPiece, posX, posY);
-    }
-
-    void checkIfClicked(){
-        if(p.mousePressed&&p.mouseX> posX &&p.mouseX< posX +80&&p.mouseY> posY &&p.mouseY< posY +80){
-            pieceHeld=true;
-            System.out.println(pieceHeld);
-            posX =p.mouseX-40;
-            posY =p.mouseY-40;
-        } else if(!p.mousePressed){
-            for(int i = 40;i<p.width/2+235;i+=100){
-                for(int t = 50;t<650;t+=100){
-                    if(posX+40>i&&posX+40<i+100){
-                        posX=i;
-                    }
-                    if(posY+40>t&&posY+40<t+100){
-                        posY=t;
-                    }
-                }
-            }
-            pieceHeld=false;
-        }
-        //p.rect(posX,posY,80,80);
-
-
-
-
+        this.p = p;
     }
 }
