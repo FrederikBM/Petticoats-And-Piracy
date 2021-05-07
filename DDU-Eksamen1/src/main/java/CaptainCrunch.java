19ff02dc806b90c0a1071f0768a23572793ea68e
FrederikBM <Qe679MNc5>
2021-05-07 13:26:32 +0200

    Revert "Knappen er nu funktionel"
    This reverts commit ed0bfbc6

--- DDU-Eksamen1/src/main/java/CaptainCrunch.java
+++ DDU-Eksamen1/src/main/java/CaptainCrunch.java
@@ -19,6 +19,8 @@ public class CaptainCrunch extends PlayerPieces {
                 ||pieceHeld&&p.mouseX > OriPosX+100-tileBorder && p.mouseX < OriPosX+100+lengthX+tileBorder && p.mouseY > OriPosY-tileBorder && p.mouseY < OriPosY + lengthY+tileBorder // én gang til højre
                 ||pieceHeld&&p.mouseX > OriPosX+100-tileBorder && p.mouseX < OriPosX+100+lengthX+tileBorder && p.mouseY > OriPosY+100-tileBorder && p.mouseY < OriPosY + lengthY+100+tileBorder){ //en gang ned højre
             piecePlacer();
+        } else if (pieceHeld && p.mouseX > OriPosX - tileBorder && p.mouseX < OriPosX + lengthX + tileBorder && p.mouseY > OriPosY - tileBorder && p.mouseY < OriPosY + lengthY + tileBorder){
+            piecePlacer();
         }
     }
 }
