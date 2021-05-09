283e1564682e9592e6aac1b423e409760ba76340
FrederikBM <Qe679MNc5>
2021-05-09 15:52:51 +0200

    Im trying for my fucking life here

--- DDU-Eksamen1/src/main/java/CaptainCrunch.java
+++ DDU-Eksamen1/src/main/java/CaptainCrunch.java
@@ -11,15 +11,15 @@ public class CaptainCrunch extends PlayerPieces {
         this.PIa=PIa;
     }
 
-    void checkIfReleased(boolean held){
-        if (held&&p.mouseX > OriPosX-100-tileBorder && p.mouseX < OriPosX-100+lengthX+tileBorder && p.mouseY > OriPosY-100-tileBorder && p.mouseY < OriPosY + lengthY-100+tileBorder // én gang op venstre
-                ||held&&p.mouseX > OriPosX-100-tileBorder && p.mouseX < OriPosX-100+lengthX+tileBorder && p.mouseY > OriPosY-tileBorder && p.mouseY < OriPosY + lengthY+tileBorder // én gang til venstre
-                ||held&&p.mouseX > OriPosX-100-tileBorder && p.mouseX < OriPosX-100+lengthX+tileBorder && p.mouseY > OriPosY+100-tileBorder && p.mouseY < OriPosY + lengthY+100+tileBorder // én gang ned venstre
-                ||held&&p.mouseX > OriPosX+100-tileBorder && p.mouseX < OriPosX+100+lengthX+tileBorder && p.mouseY > OriPosY-100-tileBorder && p.mouseY < OriPosY + lengthY-100+tileBorder // én gang op højre
-                ||held&&p.mouseX > OriPosX+100-tileBorder && p.mouseX < OriPosX+100+lengthX+tileBorder && p.mouseY > OriPosY-tileBorder && p.mouseY < OriPosY + lengthY+tileBorder // én gang til højre
-                ||held&&p.mouseX > OriPosX+100-tileBorder && p.mouseX < OriPosX+100+lengthX+tileBorder && p.mouseY > OriPosY+100-tileBorder && p.mouseY < OriPosY + lengthY+100+tileBorder){ //en gang ned højre
+    void checkIfReleased(){
+        if (pieceHeld&&p.mouseX > OriPosX-100-tileBorder && p.mouseX < OriPosX-100+lengthX+tileBorder && p.mouseY > OriPosY-100-tileBorder && p.mouseY < OriPosY + lengthY-100+tileBorder // én gang op venstre
+                ||pieceHeld&&p.mouseX > OriPosX-100-tileBorder && p.mouseX < OriPosX-100+lengthX+tileBorder && p.mouseY > OriPosY-tileBorder && p.mouseY < OriPosY + lengthY+tileBorder // én gang til venstre
+                ||pieceHeld&&p.mouseX > OriPosX-100-tileBorder && p.mouseX < OriPosX-100+lengthX+tileBorder && p.mouseY > OriPosY+100-tileBorder && p.mouseY < OriPosY + lengthY+100+tileBorder // én gang ned venstre
+                ||pieceHeld&&p.mouseX > OriPosX+100-tileBorder && p.mouseX < OriPosX+100+lengthX+tileBorder && p.mouseY > OriPosY-100-tileBorder && p.mouseY < OriPosY + lengthY-100+tileBorder // én gang op højre
+                ||pieceHeld&&p.mouseX > OriPosX+100-tileBorder && p.mouseX < OriPosX+100+lengthX+tileBorder && p.mouseY > OriPosY-tileBorder && p.mouseY < OriPosY + lengthY+tileBorder // én gang til højre
+                ||pieceHeld&&p.mouseX > OriPosX+100-tileBorder && p.mouseX < OriPosX+100+lengthX+tileBorder && p.mouseY > OriPosY+100-tileBorder && p.mouseY < OriPosY + lengthY+100+tileBorder){ //en gang ned højre
             piecePlacer();
-        } else if (held && p.mouseX > OriPosX - tileBorder && p.mouseX < OriPosX + lengthX + tileBorder && p.mouseY > OriPosY - tileBorder && p.mouseY < OriPosY + lengthY + tileBorder){
+        } else if (pieceHeld && p.mouseX > OriPosX - tileBorder && p.mouseX < OriPosX + lengthX + tileBorder && p.mouseY > OriPosY - tileBorder && p.mouseY < OriPosY + lengthY + tileBorder){
             piecePlacer();
         }
     }
